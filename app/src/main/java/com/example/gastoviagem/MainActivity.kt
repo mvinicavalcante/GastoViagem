421559dcc07fbc5fdd516c8c684bdc39da51ebfc
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2024-01-02 22:44:17 -0300

    Identificando elementos do layout

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    //pego a interface toda e inicializo ela depois
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //pego o layout
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        view.id.let {
+            if (it == R.id.button_calculate) {
+                calculate()
+            }
+        }
+    }
+
+    private fun validator(): Boolean {
+        val distance = binding.editDistance.text.toString()
+        val price = binding.editPrice.text.toString()
+        val autonomy = binding.editAutonomy.text.toString()
+        var validate = true
+
+        if (distance == "") {
+            validate = false
+            binding.editDistance.error = getString(R.string.error_distancia)
+        }
+        if (price == "") {
+            validate = false
+            binding.editPrice.error = getString(R.string.error_preco)
+        }
+        if (autonomy == "") {
+            validate = false
+            binding.editAutonomy.error = getString(R.string.error_autonomia)
+        }
+        return validate
+    }
+
+    fun calculate() {
+        if(validator() == false) {
+            Toast.makeText(this, R.string.error_validacao, Toast.LENGTH_SHORT).show()
+            return
+        }
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+        val totalValue = (distance * price) / autonomy
+        binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        Toast.makeText(this, "Calculando...", Toast.LENGTH_SHORT).show()
+    }
+
 }
